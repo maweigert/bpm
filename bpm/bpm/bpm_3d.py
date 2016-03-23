fixed quite some bugs in psf calculation and bpm_rd

bpm/bpm/bpm_3d.py
@@ -130,7 +130,7 @@ def _bpm_3d(size,
     kxs = 2.*np.pi*np.fft.fftfreq(Nx,dx)
     kys = 2.*np.pi*np.fft.fftfreq(Ny,dy)
 
-    KY, KX = np.meshgrid(kxs,kys, indexing= "ij")
+    KY, KX = np.meshgrid(kys,kxs, indexing= "ij")
 
     #H0 = np.sqrt(0.j+n0**2*k0**2-KX**2-KY**2)
     H0 = np.sqrt(n0**2*k0**2-KX**2-KY**2)

bpm/psf/_focus_fields_debye.py
@@ -74,6 +74,7 @@ def focus_field_debye(shape,units,lam, NA, n0 = 1., n_integration_steps = 200):
     alphas = np.arcsin(np.array(NA)/n0)
     assert len(alphas)%2 ==0
 
+    # as we assume the psf to be symmetric, we just have to calculate each octant
     Nx = (Nx0+1)/2
     Ny = (Ny0+1)/2
     Nz = (Nz0+1)/2
@@ -90,9 +91,9 @@ def focus_field_debye(shape,units,lam, NA, n0 = 1., n_integration_steps = 200):
     p.run_kernel("debye_wolf",u_g.shape[::-1],None,
                  ex_g.data,ey_g.data,ez_g.data, u_g.data,
                  np.float32(1.),np.float32(0.),
-                 np.float32(0.),np.float32(dx*Nx),
-                 np.float32(0.),np.float32(dy*Ny),
-                 np.float32(0.),np.float32(dz*Nz),
+                 np.float32(0.5*dx),np.float32(dx*.5*(Nx0-1)),
+                 np.float32(0.5*dy),np.float32(dy*.5*(Ny0-1)),
+                 np.float32(0.5*dz),np.float32(dz*.5*(Nz0-1)),
                  np.float32(lam),
                  np.float32(n0),
                  alpha_g.data, np.int32(len(alphas)))
@@ -109,19 +110,35 @@ def focus_field_debye(shape,units,lam, NA, n0 = 1., n_integration_steps = 200):
     ey_all = np.empty((Nz0,Ny0,Nx0),np.complex64)
     ez_all = np.empty((Nz0,Ny0,Nx0),np.complex64)
 
-    sx = [slice(0,Nx),slice(Nx0-Nx0/2,Nx0)]
-    sy = [slice(0,Ny),slice(Ny0-Ny0/2,Ny0)]
-    sz = [slice(0,Nz),slice(Nz0-Nz0/2,Nz0)]
+    # sx = [slice(0,Nx),slice(Nx0-Nx0/2,Nx0)]
+    # sy = [slice(0,Ny),slice(Ny0-Ny0/2,Ny0)]
+    # sz = [slice(0,Nz),slice(Nz0-Nz0/2,Nz0)]
 
     sx = [slice(0,Nx),slice(Nx0-Nx,Nx0)]
     sy = [slice(0,Ny),slice(Ny0-Ny,Ny0)]
     sz = [slice(0,Nz),slice(Nz0-Nz,Nz0)]
 
+    # spreading the calculated octant to the full volume
     for i,j,k in itertools.product([0,1],[0,1],[0,1]):
         u_all[sz[1-i],sy[1-j],sx[1-k]] = u[::(-1)**i,::(-1)**j,::(-1)**k]
-        ex_all[sz[1-i],sy[1-j],sx[1-k]] = ex[::(-1)**i,::(-1)**j,::(-1)**k]
-        ey_all[sz[1-i],sy[1-j],sx[1-k]] = ey[::(-1)**i,::(-1)**j,::(-1)**k]
-        ez_all[sz[1-i],sy[1-j],sx[1-k]] = ez[::(-1)**i,::(-1)**j,::(-1)**k]
+        # if i ==1:
+        #     ex_all[sz[1-i],sy[1-j],sx[1-k]] = ex[::(-1)**i,::(-1)**j,::(-1)**k]
+        #     ey_all[sz[1-i],sy[1-j],sx[1-k]] = ey[::(-1)**i,::(-1)**j,::(-1)**k]
+        #     ez_all[sz[1-i],sy[1-j],sx[1-k]] = ez[::(-1)**i,::(-1)**j,::(-1)**k]
+        # else:
+        #     print "conjugate!"
+        #     ex_all[sz[1-i],sy[1-j],sx[1-k]] = np.conjugate(ex[::(-1)**i,::(-1)**j,::(-1)**k])
+        #     ey_all[sz[1-i],sy[1-j],sx[1-k]] = np.conjugate(ey[::(-1)**i,::(-1)**j,::(-1)**k])
+        #     ez_all[sz[1-i],sy[1-j],sx[1-k]] = np.conjugate(ez[::(-1)**i,::(-1)**j,::(-1)**k])
+        if i ==1:
+            ex_all[sz[1-i],sy[1-j],sx[1-k]] = ex[::(-1)**i,::(-1)**j,::(-1)**k]
+            ey_all[sz[1-i],sy[1-j],sx[1-k]] = ey[::(-1)**i,::(-1)**j,::(-1)**k]
+            ez_all[sz[1-i],sy[1-j],sx[1-k]] = ez[::(-1)**i,::(-1)**j,::(-1)**k]
+
+        else:
+            ex_all[sz[1-i],sy[1-j],sx[1-k]] = np.conjugate(ex[::(-1)**i,::(-1)**j,::(-1)**k])
+            ey_all[sz[1-i],sy[1-j],sx[1-k]] = np.conjugate(ey[::(-1)**i,::(-1)**j,::(-1)**k])
+            ez_all[sz[1-i],sy[1-j],sx[1-k]] = np.conjugate(ez[::(-1)**i,::(-1)**j,::(-1)**k])
 
         
     return u_all, ex_all, ey_all, ez_all

tests/test_focus.py
@@ -4,46 +4,46 @@ import numpy as np
 import numpy.testing as npt
 
 
-from bpm import bpm_3d
+from bpm import bpm_3d, psf, psf_u0
 
-from bpm import bpm_3d, focus_field_debye, psf_focus_u0
-
-def test_focus(NA = .3, n0 = 1., Nx = 256, Nz = 256):
+def test_focus(size, units, NA = .3, n0 = 1.):
     """ propagates a focused wave freely to the center
     """
 
+    Nx, Ny, Nz = size
 
-    dx, dz = .1, 0.1
+    dx, dy , dz = .1, .1, .1
 
     lam = .5
 
-    units = (dx,dx,dz)
-
-    _,u_debye,  _, _ = focus_field_debye((Nx, Nx, Nz), units, lam=lam, NAs=[0., NA])
+    _,u_debye,  _, _ = psf(size, units, n0= n0, lam=lam, NA=NA, return_field = True)
 
     u0 = u_debye[0]
+    u0 = psf_u0(size[:2],units[:2],zfoc = .5*units[-1]*(size[-1]-1), lam = lam, NA = NA)
 
-    u = bpm_3d((Nx,Nx,Nz),units= units, lam = lam,
+    u = bpm_3d(size,units= units, lam = lam,
                    n0 = n0,
                    u0 = u0,
-                    dn = np.zeros((Nz,Nx,Nx)),
-                    absorbing_width = 10)
+                   absorbing_width = 0)
 
-    return u_debye, u
+    return u, u_debye
 
 if __name__ == '__main__':
 
-    # u1,u2 = test_plane(n_x_comp=1, n0 = 1.1)
 
-    Nx  = 512
+    Nx  = 256
+    Ny = 512
     Nz = 256
 
-    NAs = [0.2,.4, .6, .8]
-    n0s = [1.,1.,1.,1.]
+    dx, dy, dz = .1, .1, .1
+
+
+    NAs = [0.2,.4, .6]
+    n0s = [1.,1.2,1.4]
 
     u_bpm, u_anal = [],[]
     for NA,n0 in zip(NAs, n0s):
-        u1,u2 = test_focus(NA = NA, n0 = n0, Nx = Nx, Nz = Nz)
+        u1,u2 = test_focus((Nx,Ny,Nz),NA = NA, n0 = n0)
         u_bpm.append(u1)
         u_anal.append(u2)
 
@@ -59,8 +59,8 @@ if __name__ == '__main__':
     pylab.clf()
     for i in range(n):
         pylab.subplot(n,1,i+1)
-        pylab.plot(np.real(u_anal[i][:,Nx/2,Nx/2]), "-",c = col[1],  label="analy")
-        pylab.plot(np.real(u_bpm[i][:,Nx/2,Nx/2]), ".:", c = col[0], label="bpm")
+        pylab.plot(np.real(u_anal[i][:,Ny/2,Nx/2]), "-",c = col[1],  label="analy")
+        pylab.plot(np.real(u_bpm[i][:,Ny/2,Nx/2]), ".:", c = col[0], label="bpm")
 
         pylab.legend()
         pylab.title("NA = %s, n0 = %.2f"%(NAs[i],n0s[i]))
@@ -69,13 +69,13 @@ if __name__ == '__main__':
     pylab.clf()
     for i in range(n):
         pylab.subplot(n,2,2*i+1)
-        pylab.imshow(np.real(u_anal[i][:,Nx/2,:]), cmap = "hot")
+        pylab.imshow(np.real(u_anal[i][:,Ny/2,:]), cmap = "hot")
         pylab.grid("off")
         pylab.axis("off")
         pylab.title("anal,  NA = %s, n0 = %.2f"%(NAs[i],n0s[i]))
 
         pylab.subplot(n,2,2*i+2)
-        pylab.imshow(np.real(u_bpm[i][:,Nx/2,:]), cmap = "hot")
+        pylab.imshow(np.real(u_bpm[i][:,Ny/2,:]), cmap = "hot")
         pylab.grid("off")
         pylab.axis("off")
         pylab.title("bpm,  NA = %s, n0 = %.2f"%(NAs[i],n0s[i]))
@@ -100,27 +100,20 @@ if __name__ == '__main__':
     pylab.clf()
     for i in range(n):
         pylab.subplot(n,2,2*i+1)
-        pylab.imshow(np.abs(u_anal[i][:,Nx/2,:]), cmap = "hot")
+        pylab.imshow(np.abs(u_anal[i][:,Ny/2,:]), cmap = "hot")
         pylab.grid("off")
         pylab.axis("off")
         pylab.title("anal,  NA = %s, n0 = %.2f"%(NAs[i],n0s[i]))
 
         pylab.subplot(n,2,2*i+2)
-        pylab.imshow(np.abs(u_bpm[i][:,Nx/2,:]), cmap = "hot")
+        pylab.imshow(np.abs(u_bpm[i][:,Ny/2,:]), cmap = "hot")
         pylab.grid("off")
         pylab.axis("off")
         pylab.title("bpm,  NA = %s, n0 = %.2f"%(NAs[i],n0s[i]))
 
 
-    # pylab.subplot(2,1,2)
-    # pylab.title("x_comp = 2")
-    # pylab.plot(np.real(a1)[:,64,64], "-", c = col[1], label="2 bpm")
-    # pylab.plot(np.real(a2)[:,64,64], ".",c = col[1],  label="2 analy")
-
 
     pylab.legend()
     pylab.show()
     pylab.draw()
-    # test_plane(1)
-    # test_plane(2)
-    
+

tests/test_plane.py
@@ -8,11 +8,11 @@ from bpm import bpm_3d
 
 from bpm import bpm_3d
 
-def test_plane(n_x_comp = 0, n0 = 1., n = None, N = 256):
+def test_plane(size = (256,)*3, n_x_comp = 0, n0 = 1., n = None):
     """ propagates a plane wave freely
     n_x_comp is the tilt in x
     """
-    Nx, Ny, Nz = (N,)*3
+    Nx, Ny, Nz = size
 
     dx, dz = .05, 0.05
 
@@ -31,41 +31,43 @@ def test_plane(n_x_comp = 0, n0 = 1., n = None, N = 256):
     
     k_x = 1.*n_x_comp/(dx*(Nx-1.))
 
-    
-    k_z = np.sqrt(1.*n**2/lam**2-k_x**2)
+    k_y = k_x
 
-    print (k_x,k_z), np.sqrt(k_x**2+k_z**2)
     
-    u_plane = np.exp(-2.j*np.pi*(k_z*Z+k_x*X))
+    k_z = np.sqrt(1.*n**2/lam**2-k_x**2-k_y**2)
+
+    print (k_x,k_z), np.sqrt(k_x**2+k_y**2+k_z**2)
+    
+    u_plane = np.exp(-2.j*np.pi*(k_z*Z+k_y*Y+k_x*X))
 
     u = 0
     dn = (n-n0)*np.ones_like(Z)
 
-    print n,n0, np.mean(dn)
 
-
-    print "start"
     u = bpm_3d((Nx,Ny,Nz),units= units, lam = lam,
                    n0 = n0,
                    dn = dn,
                    n_volumes = 1,
-                   absorbing_width=10,
+                   absorbing_width=0,
                    u0 = u_plane[0,...])
 
+    print "L2 difference = %.4f"%np.mean(np.abs(u_plane-u)**2)
     #npt.assert_almost_equal(np.mean(np.abs(u_plane-u)**2),0,decimal = 2)
     return u, u_plane
 
 if __name__ == '__main__':
 
-    # u1,u2 = test_plane(n_x_comp=1, n0 = 1.1)
 
-    Nx  =128
+    Nx = 256
+    Ny = 128
+    Nz = 128
+
     x_comps = [0,2]
-    n0s = [1.2,1.]
+    n0s = [1.,1.2]
 
     u_bpm, u_plane = [],[]
     for x_comp,n0 in zip(x_comps, n0s):
-        u1,u2 = test_plane(n_x_comp=x_comp, n0 = n0, N = Nx)
+        u1,u2 = test_plane((Nx,Ny,Nz),n_x_comp=x_comp, n0 = n0)
         u_bpm.append(u1)
         u_plane.append(u2)
 
@@ -81,8 +83,8 @@ if __name__ == '__main__':
     pylab.clf()
     for i in range(n):
         pylab.subplot(n,1,i+1)
-        pylab.plot(np.real(u_plane[i][:,Nx/2,Nx/2]), "-",c = col[1],  label="analy")
-        pylab.plot(np.real(u_bpm[i][:,Nx/2,Nx/2]), ".:", c = col[0], label="bpm")
+        pylab.plot(np.real(u_plane[i][:,Ny/2,Nx/2]), "-",c = col[1],  label="analy")
+        pylab.plot(np.real(u_bpm[i][:,Ny/2,Nx/2]), ".:", c = col[0], label="bpm")
 
         pylab.legend()
         pylab.title("x_comp = %s, n0 = %.2f"%(x_comps[i],n0s[i]))
@@ -91,12 +93,12 @@ if __name__ == '__main__':
     pylab.clf()
     for i in range(n):
         pylab.subplot(n,2,2*i+1)
-        pylab.imshow(np.real(u_plane[i][:,Nx/2,:]), cmap = "hot")
+        pylab.imshow(np.real(u_plane[i][:,Ny/2,:]), cmap = "hot")
         pylab.grid("off")
         pylab.title("anal,  x_comp = %s, n0 = %.2f"%(x_comps[i],n0s[i]))
 
         pylab.subplot(n,2,2*i+2)
-        pylab.imshow(np.real(u_bpm[i][:,Nx/2,:]), cmap = "hot")
+        pylab.imshow(np.real(u_bpm[i][:,Ny/2,:]), cmap = "hot")
         pylab.grid("off")
         pylab.title("bpm,  x_comp = %s, n0 = %.2f"%(x_comps[i],n0s[i]))
 